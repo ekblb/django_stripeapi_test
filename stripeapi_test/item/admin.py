1708ba6b2f809c15f41b020bdf306ce8e74bf9a8
Екатерина Балабаева <balabaeva.e.yu@yandex.ru>
2023-12-21 18:03:04 +0300

    Add Item model, admin panel for Item model

--- stripeapi_test/item/admin.py
+++ stripeapi_test/item/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from item.models import Item
 
-# Register your models here.
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'price')
+    list_filter = ('name')
--- stripeapi_test/item/models.py
+++ stripeapi_test/item/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.core import validators
+from item.constants import MAX_LENGTH_ITEM, MIN_VALUE_PRICE, MAX_VALUE_PRICE
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=MAX_LENGTH_ITEM,
+        unique=True,
+    )
+    description = models.TextField(
+        verbose_name='Описание',
+    )
+    price = models.PositiveIntegerField(
+        verbose_name='Цена',
+        validators=[
+            validators.MinValueValidator(
+                limit_value=MIN_VALUE_PRICE,
+                message='Значение должно быть больше 1.',
+            ),
+            validators.MaxValueValidator(
+                limit_value=MAX_VALUE_PRICE,
+                message='Значение должно быть меньше 2147483647.',
+                )
+        ]
+    )
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+        ordering = ('name',)
+        constraints = [
+            models.UniqueConstraint(
+                fields=('name', 'description'),
+                name='unique_name_description'
+            )
+        ]
+
+    def __str__(self) -> str:
+        return self.name
